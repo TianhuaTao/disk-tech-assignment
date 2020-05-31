fe4bc50bf47bdab9d3626a6453cc5c0ee862e4ef
chenzm-plusplus <chenzm17@mails.tsinghua.edu.cn>
2020-05-31 04:48:42 -0700

    Change: apart file system from networking client

--- fuse_fs/zerodrive/DriveClientAgent.cpp
+++ fuse_fs/zerodrive/DriveClientAgent.cpp
@@ -15,6 +15,213 @@
 #include <iostream>
 #include "op.h"
 #include "Protocol.h"
+
+#include "DriveClientAgent.h"
+
+DriveClientAgent::DriveClientAgent(const char *address, int port) {
+    networkAgent = new NetworkAgent();
+    networkAgent->setRole(NetworkAgent::SERVER);
+    networkAgent->listenAsync(address, port, []() {
+        std::cout << "Got connection\n";
+    });
+    fileOperation = new FileOperation();
+    printf("NetworkAgent init complete\n");
+}
+
+DriveClientAgent::~DriveClientAgent() {
+    delete networkAgent;
+    delete fileOperation;
+    printf("Server agent shut down\n");
+}
+
+void *DriveClientAgent::Init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
+    return fileOperation->Init(conn,cfg);
+
+    /*
+    (void) conn;
+    cfg->use_ino = 1;
+
+    /// Pick up changes from lower filesystem right away. This is
+	   also necessary for better hardlink support. When the kernel
+	   calls the unlink() handler, it does not know the inode of
+	   the to-be-removed entry and can therefore not invalidate
+	   the cache of the associated inode - resulting in an
+	   incorrect st_nlink value being reported for any remaining
+	   hardlinks to this inode. //
+    cfg->entry_timeout = 0;
+    cfg->attr_timeout = 0;
+    cfg->negative_timeout = 0;
+
+    printf("data path: %s\n", get_data_dir());
+
+    // TODO: check failure
+    if (mkdir(get_data_dir(), 0777) == -1) {
+        if (errno == EEXIST) {
+            // already exists
+            printf("found previous data\n");
+        } else {
+            printf("cannot create data folder\n");
+        }
+    }
+
+    return nullptr;
+    */
+}
+
+//-------------------init-------------------------------------------------------
+
+int DriveClientAgent::Read(const char *path, char *buf, size_t size, 
+                            off_t offset, struct fuse_file_info *fi) {
+    return fileOperation->Read(path, buf, size, offset, fi);
+    // send signal to client
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(path));
+    //this->broadcastChanges(WRITE_DONE, detail);
+}
+
+int DriveClientAgent::Getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
+    return fileOperation->Getattr(path,stbuf,fi);
+    //TODO: send signal to client
+    // send signal to client
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(path));
+    //this->broadcastChanges(WRITE_DONE, detail);
+}
+
+//---------------------will do something-----------------------------------------------------------------
+
+int DriveClientAgent::Write(const char *path, const char *buf, size_t size, 
+                        off_t offset, struct fuse_file_info *fi) {
+    // send signal to client
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(path));
+    //this->broadcastChanges(WRITE_DONE, detail);
+    return fileOperation->Write(path, buf, size, offset, fi);
+}
+
+int DriveClientAgent::Rename(const char *from, const char *to, unsigned int flags) {
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(from));
+    detail.push_back(std::string(to));
+    //this->broadcastChanges(RENAME, detail);
+    return fileOperation->Rename(from, to, flags);
+
+}
+
+int DriveClientAgent::Open(const char *path, struct fuse_file_info *fi) {
+    return fileOperation->Open(path,fi);
+
+    //TODO: send signal to client
+    // send signal to client
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(path));
+    //this->broadcastChanges(WRITE_DONE, detail);
+}
+
+int DriveClientAgent::Readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi,
+                          enum fuse_readdir_flags flags) {
+    return fileOperation->Readdir(path,buf,filler, offset,fi,flags);
+
+    //TODO: before I read the directory, I should keep the info updated
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(path));
+    //this->broadcastChanges(WRITE_DONE, detail);
+}
+
+int DriveClientAgent::Create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(path));
+    //detail.push_back(std::string(mode));
+    //this->broadcastChanges(CREATE, detail);
+    return fileOperation->Create(path,mode,fi);
+}
+
+int DriveClientAgent::Mkdir(const char *path, mode_t mode) {
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(path));
+    //detail.push_back(std::string(mode));
+    //this->broadcastChanges(MKDIR, detail);
+
+    return fileOperation->Mkdir(path, mode);
+}
+
+int DriveClientAgent::Rmdir(const char *path) {
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(path));
+    //this->broadcastChanges(RMDIR, detail);
+    return fileOperation->Rmdir(path);
+}
+
+int DriveClientAgent::Symlink(const char *from, const char *to) {
+    return fileOperation->Symlink(from, to);
+    // TODO: send signal to client
+    //what will symlink do?????????
+    //std::vector<std::string> detail;
+    //detail.push_back(std::string(from));
+    //detail.push_back(std::string(to));
+    //this->broadcastChanges(WRITE_DONE, detail);
+}
+
+int DriveClientAgent::Chmod(const char *path, mode_t mode, struct fuse_file_info *fi) {
+
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(path));
+    //detail.push_back(std::string(mode));
+    //this->broadcastChanges(CHMOD, detail);
+
+    return fileOperation->Chmod(path, mode, fi);
+}
+
+int DriveClientAgent::Chown(const char *path, uid_t uid, gid_t gid, struct fuse_file_info *fi) {
+    // TODO: send signal to client
+    // send signal to client
+    std::vector<std::string> detail;
+    detail.push_back(std::string(path));
+    //detail.push_back(std::string(uid));
+    //detail.push_back(std::string(gid));
+    //this->broadcastChanges(CHOWN, detail);
+
+    return fileOperation->Chown(path, uid, gid, fi);
+}
+
+int DriveClientAgent::Readlink(const char *path, char *buf, size_t size) {
+    return fileOperation->Readlink(path, buf, size);
+
+    // send signal to client
+    //????????????but what will this operation do???
+    return 0;
+}
+
+
+/*
+#define FUSE_USE_VERSION 31
+
+
+#include "DriveAgent.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <cassert>
+#include <dirent.h>
+#include <cerrno>
+#include <cstring>
+#include <sys/types.h>
+#include <cstdio>
+#include <pwd.h>
+#include <unistd.h>
+#include "NetworkAgent.h"
+#include <iostream>
+#include "op.h"
+#include "Protocol.h"
 #include "DriveClientAgent.h"
 
 DriveClientAgent::DriveClientAgent(const char *address, int port) {
@@ -23,6 +230,8 @@ DriveClientAgent::DriveClientAgent(const char *address, int port) {
     networkAgent->connectAsync(address, port, []() {
         std::cout << "Callback: Got connection\n";
     });
+
+    fileOperation = new FileOperation();
 
     printf("NetworkAgent init complete\n");
     // TODO: connect to server
@@ -34,16 +243,16 @@ DriveClientAgent::~DriveClientAgent() {
 
 void *DriveClientAgent::Init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
     // TODO: implement this function
-        (void) conn;
+    (void) conn;
     cfg->use_ino = 1;
 
-    /* Pick up changes from lower filesystem right away. This is
+    Pick up changes from lower filesystem right away. This is
 	   also necessary for better hardlink support. When the kernel
 	   calls the unlink() handler, it does not know the inode of
 	   the to-be-removed entry and can therefore not invalidate
 	   the cache of the associated inode - resulting in an
 	   incorrect st_nlink value being reported for any remaining
-	   hardlinks to this inode. */
+	   hardlinks to this inode. 
     cfg->entry_timeout = 0;
     cfg->attr_timeout = 0;
     cfg->negative_timeout = 0;
@@ -310,3 +519,4 @@ int DriveClientAgent::Readlink(const char *path, char *buf, size_t size) {
     //TODO: send signal to client
     return 0;
 }
+*/
